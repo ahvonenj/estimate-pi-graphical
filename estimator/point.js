776c2076feb895187057d174faf77caa8e5b2de8
Jonah <jonah.ahvonen@gmail.com>
2016-04-27 19:25:27 +0300

    Accurate drawing

--- estimator/point.js
+++ estimator/point.js
@@ -12,6 +12,8 @@ function Point(x, y, sprite, estimator)
 	this.sprite.position.x = this.x;
 	this.sprite.position.y = this.y;
 
+	this.sprite.anchor = new PIXI.Point(0.5, 0.5);
+
     this.estimator.stage.addChild(this.sprite);
 }
 
